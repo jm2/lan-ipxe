979a16706c71b0eb35e2d67dac2311848c2ad06e
John-Michael Mulesa <jmulesa@gmail.com>
2026-02-27 22:22:48 -0500

    Expand URL matcher.

--- mlnx-fw-flash-update.sh
+++ mlnx-fw-flash-update.sh
@@ -148,13 +148,67 @@ TARGET_FAMILY=$(get_mlx_family "$TARGET_DEV_ID")
 TARGET_OEM=$(get_oem_from_psid "$TARGET_PSID")
 
 get_fw_url() {
-    case "${1}" in
-        "ConnectX-3"|"ConnectX-3 Pro") echo "https://network.nvidia.com/support/firmware/connectx3pro/" ;;
-        "ConnectX-4") echo "https://network.nvidia.com/support/firmware/connectx4/" ;;
-        "ConnectX-4 Lx") echo "https://network.nvidia.com/support/firmware/connectx4lx/" ;;
-        "ConnectX-5") echo "https://network.nvidia.com/support/firmware/connectx5/" ;;
-        "ConnectX-5 Ex") echo "https://network.nvidia.com/support/firmware/connectx5en/" ;;
-        "ConnectX-6") echo "https://network.nvidia.com/support/firmware/connectx6/" ;;
+    local pn="$1"
+    local family="$2"
+    
+    # 1. Precise Match by Part Number (OPN)
+    case "$pn" in
+        "MCX75343AAS-NEA" | "MCX75510AAS-NEA" | "MCX753436MS-HEA" | "MCX755106AS-HEA" | "MCX75310AAS-NEA" | "MCX75510AAS-HEA" | "MCX713106AS-VEA" | "MCX713106AC-VEA" | "MCX713106AC-CEA" | "MCX713106AS-CEA" | "MCX75310AAS-HEA" | "MCX713104AS-ADA" | "MCX75210AAS-HEA" | "MCX75210AAS-NEA" | "MCX713104AC-ADA" | "MCX750500B-0D0K" | "MCX750500C-0D0K" | "MCX750500B-0D00" | "MCX750500C-0D00" | "MCX755206AS-NEA-N" | "MCX753436MC-HEA" | "MCX755106AC-HEA" | "MCX75310AAC-NEA" | "MCX713114TC-GEA" | "MCX75343AMS-NEAC" | "MCX75343AMC-NEAC" | "692-9X760-00SE-S00" | "692-9X760-00SE-S0C" | "MCX753436MS-HEB" | "MCX715105AS-WEAT" | "MCX753106AS-HEA-N" | "MCX75310AAS-HEA-N" | "P3740-B0-QSFP" | "900-24768-0002")
+            echo "https://network.nvidia.com/support/firmware/connectx7/"
+            ;;
+        "MCX683105AN-HDA")
+            echo "https://network.nvidia.com/support/firmware/connectx6de/"
+            ;;
+        "MCX631102AS-ADA" | "MCX631105AE-GDA" | "MCX631102AN-ADA" | "MCX631432AN-ADA" | "MCX631432AC-ADA" | "MCX631435AN-GDA" | "MCX631435AC-GDA" | "MCX631432AS-ADA" | "MCX631105AC-GDA" | "MCX631102AE-ADA" | "MCX631432AE-ADA" | "MCX631435AE-GDA" | "MCX631105AN-GDA" | "MCX631102AC-ADA")
+            echo "https://network.nvidia.com/support/firmware/connectx6lx/"
+            ;;
+        "MCX623102AC-GDA" | "MCX623106PN-CDA" | "MCX623102AC-ADA" | "MCX623432AN-GDA" | "MCX623436AN-CDA" | "MCX623106AN-CDA" | "MCX623102AE-GDA" | "MCX623106TC-CDA" | "MCX623436MN-CDA" | "MCX623430MS-CDA" | "MCX623432MN-ADA" | "MCX621202AC-ADA" | "MCX623435AC-CDA" | "MCX623106AC-CDA" | "MCX623106AS-CDA" | "MCX623436AE-CDA" | "MCX623435AC-VDA" | "MCX621102AN-ADA" | "MCX623432AN-ADA" | "MCX623106PC-CDA" | "MCX623435AN-VDA" | "MCX621102AE-ADA" | "MCX623106PE-CDA" | "MCX623435AE-CDA" | "MCX623102AS-GDA" | "MCX623105AN-CDA" | "MCX623105AS-VDA" | "MCX623436AS-CDA" | "MCX623432AS-ADA" | "MCX623102AS-ADA" | "MCX623435AN-CDA" | "MCX623432AE-ADA" | "MCX623102AN-GDA" | "MCX623435MN-VDA" | "MCX623439MC-CDA" | "MCX623105AE-CDA" | "MCX623106GN-CDA" | "MCX623432AC-GDA" | "MCX623405AN-CDA" | "MCX621102AC-ADA" | "MCX623105AN-VDA" | "MCX623106TS-CDA" | "MCX623436AC-CDA" | "MCX623105AC-VDA" | "MCX623435AS-VDA" | "MCX623432AS-GDA" | "MCX623106TN-CDA" | "MCX623106GC-CDA" | "MCX623436MS-CDA" | "MCX623432AC-ADA" | "MCX623405AC-CDA" | "MCX623435MN-CDA" | "MCX623106AE-CDA" | "MCX623105AE-VDA" | "MCX623105AC-CDA" | "MCX621202AS-ADA" | "MCX623102AN-ADA" | "MCX623405AN-VDA")
+            echo "https://network.nvidia.com/support/firmware/connectx6dx/"
+            ;;
+        "MCX613105A-VDA" | "MCX614106A-VCA" | "MCX613106A-VDA" | "MCX614106A-CCA" | "MCX613106A-CCA" | "MCX614105A-VCA" | "MCX613436A-VDA")
+            echo "https://network.nvidia.com/support/firmware/connectx6en/"
+            ;;
+        "MCX654106A-HCA" | "MCX653435A-HDA" | "MCX653435M-HDA" | "MCX653106A-HDA" | "MCX654105A-HCA" | "MCX653106A-ECA" | "MCX653436A-HDA" | "MCX653105A-ECA" | "MCX653105A-HDA" | "MCX653105A-EFA" | "MCX653435A-EDA" | "MCX654106A-ECA" | "MCX653106A-EFA" | "MCX651105A-EDA")
+            echo "https://network.nvidia.com/support/firmware/connectx6ib/"
+            ;;
+        "MCX565M-CDA" | "MCX512A-ACA" | "MCX545A-CCU" | "MCX545B-CCU" | "MCX542A-ACA" | "MCX545B-GCU" | "MCX516A-CCA" | "MCX546A-CDAN" | "MCX542B-ACA" | "MCX516A-CCH" | "MCX512A-ADA" | "MCX562A-ACA" | "MCX511F-ACA" | "MCX546A-BCAN" | "MCX566A-CCA" | "MCX512F-ACA" | "MCX512F-ACH" | "MCX566A-CDA" | "MCX566M-GDA" | "MCX516A-BDA" | "MCX512A-ACU" | "MCX513A-GCH" | "MCX515A-CCU" | "MCX542B-ACU" | "MCX565A-CCA" | "MCX514A-GCH" | "MCX516A-GCA" | "MCX545A-CCA" | "MCX516A-CDA" | "MCX515A-CCA" | "MCX515A-GCA")
+            echo "https://network.nvidia.com/support/firmware/connectx5en/"
+            ;;
+        "MCX556A-EDA" | "MCX545B-ECA" | "MCX555A-ECA" | "MCX556A-ECU" | "MCX553Q-ECA" | "MCX556A-ECA" | "MCX545M-ECA" | "MCX546A-EDAN" | "MCX556M-ECA" | "MCX545A-ECA")
+            echo "https://network.nvidia.com/support/firmware/connectx5ib/"
+            ;;
+        "MCX4121A-XCA" | "MCX4121A-ACA" | "MCX4131A-GCA" | "MCX4411A-ACA" | "MCX4421A-ACQ" | "MCX4431A-GCU" | "MCX4411A-ACUN" | "MCX4421A-ACU" | "MCX4111A-ACA" | "MCX4131A-BCA" | "MCX4411A-ACQ" | "MCX4421A-ACA" | "MCX4121A-XCH" | "MCX4621A-XCA" | "MCX4621A-ACA" | "MCX4411A-ACH" | "MCX4121A-ACU" | "MCX4111A-ACUT" | "MCX4111A-XCA" | "MCX4421A-XCQ" | "MCX4421A-XCH" | "MCX4431A-GCA" | "MCX4431M-GCA" | "MCX4121A-ACH")
+            echo "https://network.nvidia.com/support/firmware/connectx4lxen/"
+            ;;
+        "MCX414A-BCA" | "MCX416A-CCA" | "MCX413A-GCA" | "MCX414A-GCA" | "MCX413A-BCA")
+            echo "https://network.nvidia.com/support/firmware/connectx4en/"
+            ;;
+        "MCX455A-FCA" | "MCX456A-ECA" | "MCX454A-FCA" | "MCX456A-FCA" | "MCX453A-FCA")
+            echo "https://network.nvidia.com/support/firmware/connectx4ib/"
+            ;;
+        "MCX311A-XCCT" | "MCX312B-XCCT" | "MCX312C-XCCT" | "MCX313A-BCCT" | "MCX314A-BCCT" | "MCX341A-XCQN" | "MCX341A-XCPN" | "MCX342A-XCQN" | "MCX342A-XCPN" | "MCX345A-BCPN" | "MCX345A-BCQN" | "MCX346A-BCPN" | "MCX346A-BCQN")
+            echo "https://network.nvidia.com/support/firmware/connectx3proen/"
+            ;;
+        "MCX353A-FCCT" | "MCX354A-FCCT")
+            echo "https://network.nvidia.com/support/firmware/connectx3proib/"
+            ;;
+        "MCX311A-XCAT" | "MCX312A-XCBT" | "MCX312B-XCBT" | "MCX313A-BCBT" | "MCX314A-BCBT" | "MCX341A-XCGN" | "MCX342A-XCCN" | "MCX342A-XCGN" | "MCX341A-XCCN")
+            echo "https://network.nvidia.com/support/firmware/connectx3en/"
+            ;;
+        "MCX353A-FCBT" | "MCX353A-QCBT" | "MCX353A-TCBT" | "MCX354A-FCBT" | "MCX354A-QCBT" | "MCX354A-TCBT")
+            echo "https://network.nvidia.com/support/firmware/connectx3ib/"
+            ;;
+    esac
+    
+    # 2. Fallback Match by Family
+    case "$family" in
+        "ConnectX-3") echo "https://network.nvidia.com/support/firmware/connectx3ib/" ;;
+        "ConnectX-3 Pro") echo "https://network.nvidia.com/support/firmware/connectx3proib/" ;;
+        "ConnectX-4") echo "https://network.nvidia.com/support/firmware/connectx4ib/" ;;
+        "ConnectX-4 Lx") echo "https://network.nvidia.com/support/firmware/connectx4lxen/" ;;
+        "ConnectX-5") echo "https://network.nvidia.com/support/firmware/connectx5ib/" ;;
+        "ConnectX-5 Ex") echo "https://network.nvidia.com/support/firmware/connectx5ib/" ;;
+        "ConnectX-6") echo "https://network.nvidia.com/support/firmware/connectx6ib/" ;;
         "ConnectX-6 Dx") echo "https://network.nvidia.com/support/firmware/connectx6dx/" ;;
         "ConnectX-6 Lx") echo "https://network.nvidia.com/support/firmware/connectx6lx/" ;;
         "ConnectX-7") echo "https://network.nvidia.com/support/firmware/connectx7/" ;;
@@ -162,7 +216,7 @@ get_fw_url() {
     esac
 }
 
-NVIDIA_URL=$(get_fw_url "$TARGET_FAMILY")
+NVIDIA_URL=$(get_fw_url "$TARGET_PN" "$TARGET_FAMILY")
 
 echo ""
 echo "============================================================"
